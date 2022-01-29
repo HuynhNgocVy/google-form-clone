2404329c48124f21679cc3fee6d6907d9df58dc2
HuynhNgocVy <huynhngocvy2027@gmail.com>
2022-01-29 22:37:29 +0700

    Survey

--- src/arrayQuestion.js
+++ src/arrayQuestion.js
@@ -1,20 +1,26 @@
 import  question1 from '../src/images/question1.png'
 import  question2 from '../src/images/question2.png'
 import  question3 from '../src/images/question3.png'
+import  question4 from '../src/images/question4.png'
+import  question5 from '../src/images/question5.png'
+import  question6 from '../src/images/question6.png'
+import  question7 from '../src/images/question7.png'
+import  question8 from '../src/images/question8.png'
+import  question9 from '../src/images/question9.png'
 import  asnout from '../src/images/asnout.png'
 
 const questions = [
     {
         id: 1,
         question: "What is your gender?",
-        image: question1,
+        image: question5,
         answers: ["Male", "Female", "Prefer not to say"],
         isRadio: true
     },
     {
         id: 2,
         question: "What is your age?",
-        image: question2,
+        image: question4,
         answers: ["Under 18", "18-24 years old", "25-34 years old", "25-34 years old",
          "35-44 years old", "45 or older"],
         isRadio: true
@@ -22,7 +28,7 @@ const questions = [
     {
         id: 3,
         question: "Please specify your ethnicity?",
-        image: question3,
+        image: question7,
         answers: ["Asian / Pacific Islander", "Hispanic or Latino", "Native American or American Indian", "White",
         "Black or African American"
         ],
@@ -32,7 +38,7 @@ const questions = [
     {
         id: 4,
         question: "What department do you work in?",
-        image: asnout,
+        image: question1,
         answers: ["Marketing", "Product development", "Customer success"],
         addAnswer: true
     },
@@ -47,14 +53,14 @@ const questions = [
     {
         id: 6,
         question: "Did you consider buying any other products on the market?",
-        image: question2,
+        image: question3,
         answers: ["Yes", "No"],
         isRadio: true,
     },
     {
         id: 7,
-        question: "what types of credit cards do you have (Select all that apply)?",
-        image: question2,
+        question: "What types of credit cards do you have (Select all that apply)?",
+        image: question6,
         answers: ["Visa", "Mastercard", "Momo", "PayPal"],
         isCheckbox: true,
         addAnswer: true
@@ -62,7 +68,7 @@ const questions = [
     {
         id: 8,
         question: "What is the capital of Uruguay?",
-        image: question2,
+        image: question8,
         answers: ["New Uruguay", "Panama City", "Monterey", "Montevideo"],
         addAnswer: true
     },
--- src/components/Right/RightSidebar.js
+++ src/components/Right/RightSidebar.js
@@ -3,8 +3,7 @@ import { questions } from "../../arrayQuestion";
 import { useEffect, useState, useContext } from 'react'
 import { KeyId } from '../Left/LeftSidebar'
 function RightSidebar({ showQuestion }) {
-    // const showQuestionById = useContext(KeyId)
-    console.log(showQuestion == questions.id)
+    // console.log(showQuestion == questions.id)
     const [isRadio, setIsRadio] = useState(false);
     const [isCheckbox, setIsCheckbox] = useState(false);
     return (
@@ -14,17 +13,17 @@ function RightSidebar({ showQuestion }) {
                 showQuestion === question.id &&
                 (<div key={question.id}>
                     <div className="flex justify-center">
-                        <img className="w-44" style={{transform: "translateY(-50px)"}}
+                        <img className="w-44" style={{transform: "translateY(-60px)"}}
                         src= {question.image}/>
                     </div>
                     <h2 className="font-bold text-lg">{question.id}. {question.question}</h2>
                     <h5 className="ml-4 text-sm text-gray-300 italic mb-4">Description (optional)</h5>
-                    {question.answers.map(answer => 
+                    {question.answers.map((answer, index) => 
                         <div className='flex text-sm mb-4 text-center rounded-lg py-2 bg-slate-50 hover:bg-slate-600 hover:text-slate-100 cursor-pointer'>
                             <div className= { (question.isRadio && "rounded-full m-3 w-4 h-4 border-gray-400 border border-solid") ||
                                             (question.isCheckbox && "m-3 w-4 h-4 border-gray-400 border border-solid") || 
                                             ('flex flex-col mx-4 w-9 h-9 rounded-md items-center justify-center')} style={{border: "2px solid #000"}}> 
-                                {question.isRadio || question.isCheckbox ? '' : 'A'}
+                                {question.isRadio || question.isCheckbox ? '' : String.fromCharCode(65 + index)}
                             </div>
                             <span className='flex text-center' 
                             style={{alignSelf:"center"}}> {answer}
